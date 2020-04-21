9bba9f4456626ec04a17b9a9d5811627bb0d2a2b
sangTran <sangthien2010@gmail.com>
2020-04-21 12:55:31 -0500

    Updates

---
+++ precache-manifest.c2db6541fadd0cc11f6e01229a5879e4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d5575bb430af785bdea7ff593698304",
+    "url": "/PWA-Robofriends-Udemy/index.html"
+  },
+  {
+    "revision": "80129a9bea8e1c2ff4b4",
+    "url": "/PWA-Robofriends-Udemy/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "20e92596e94eae1aaaef",
+    "url": "/PWA-Robofriends-Udemy/static/css/main.37daadfa.chunk.css"
+  },
+  {
+    "revision": "80129a9bea8e1c2ff4b4",
+    "url": "/PWA-Robofriends-Udemy/static/js/2.9bf63e8f.chunk.js"
+  },
+  {
+    "revision": "4a8cddea50fefad5b6f84ace2f7f0a62",
+    "url": "/PWA-Robofriends-Udemy/static/js/2.9bf63e8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20e92596e94eae1aaaef",
+    "url": "/PWA-Robofriends-Udemy/static/js/main.e4c1bb5a.chunk.js"
+  },
+  {
+    "revision": "2b4522720c8dd8668e11",
+    "url": "/PWA-Robofriends-Udemy/static/js/runtime-main.f77daf8a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/PWA-Robofriends-Udemy/static/media/SEGA.12a3db8b.woff"
+  }
+]);
